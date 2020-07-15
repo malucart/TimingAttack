7dcfc9b3c7fbaf1f035396674ec1258384761fe2
malucart <malucartaxo_@hotmail.com>
2020-07-14 17:08:12 -0700

    better signup file

--- website/signup.php
+++ website/signup.php
@@ -1,46 +1,7 @@
+<?php
+    require "header.php";
+?>
     <main>
-
-    <!-- Everything necessary to make the website pretty (framework and personal css) with a mobile style as well-->
-        <head>
-            <meta name=viewport content="width=device-width, initial-scale=1">
-            <meta charset="UTF-8">
-            <!-- This step works on mobile -->
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Login</title>
-            <!-- latest update from nes.css -->
-            <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
-            <!-- flexbox grid -->
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
-            <!-- my css -->
-            <link href="css/style.css" rel="stylesheet" />
-            <title></title>
-        </head>
-
-        <body>
-
-            <!-- First parte, which is the store's name and social media links -->
-            <header>
-                <nav class="header">
-                    <div class="container">
-                        <h2>
-                            <a class="snes-logo" href=""></a>
-                            8-bit Store
-                            <a href="#" style="float: right; margin-left: 20px;">
-                                <i class="nes-icon twitter"></i>
-                            </a>
-                            <a href="#" style="float: right; margin-left: 20px;">
-                                <i class="nes-icon instagram"></i>
-                            </a>
-                            <a href="#" style="float: right;">
-                                <i class="nes-icon facebook"></i>
-                            </a>
-                        </h2>
-                    </div>
-                </nav>
-            </header>
-
             <!-- Sing Up -->
             <div class="container">
                 <div class="col-xs-12 col-sm-12" style="margin-top: 40px;">
